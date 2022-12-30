e5b3ffa660073f0c48effb901554dde054b1bbaa
darraghmckeogh <darragh.mckeogh@paconsulting.com>
2022-12-30 10:22:18 +0000

    new

--- app/routes.js
+++ app/routes.js
@@ -25,7 +25,7 @@ router.post('/returning-boolean-answer', function (req, res) {
   
   })
 
-  // Run this code when a form is submitted to returning-code-answer
+// Run this code when a form is submitted to returning-code-answer
 router.post('/returning-code-answer', function (req, res) {
 
     // Make a variable and give it the value from 'returning-code'
@@ -38,6 +38,42 @@ router.post('/returning-code-answer', function (req, res) {
     } else {
       // Send user to ineligible page
       res.redirect('/register-task-list-returning')
+    }
+  
+  })
+
+// Run this code when a form is submitted to returning-code-answer
+router.post('/eligibility-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'number-of-floors'
+    var numberOfFloors = req.session.data['number-of-floors']
+    var buildingHeight = req.session.data['height']
+    var residentialUnits = req.session.data['number-of-residential-units']
+    var occupied = req.session.data['are-people-living-in-the-building']
+  
+    if ((Number(numberOfFloors) >= 7 || Number(buildingHeight) >= 18) && Number(residentialUnits) >= 2 && occupied !== "No and people will not be moving in"){
+      // Send user to eligible page
+      res.redirect('/register-task-list-confirm')
+    } else {
+        // Send user to not eligible page
+        res.redirect('/not-eligible')
+    }
+  
+  })
+
+  // Run this code when a form is submitted to other-name-address-postcode-boolean-answer
+router.post('/other-name-address-postcode-boolean-answer', function (req, res) {
+
+    // Make a variable and give it the value from 'returning-boolean'
+    var otherNameAddressPostcode = req.session.data['other-name-address-postcode-boolean']
+  
+    // Check whether the variable matches a condition
+    if (otherNameAddressPostcode == "Yes"){
+      // Send user to next page
+      res.redirect('/questions/building/other-name-address-postcode')
+    } else {
+      // Send user to ineligible page
+      res.redirect('/questions/building/completion-year')
     }
   
   })
